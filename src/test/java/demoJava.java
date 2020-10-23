640e8b0638f2df3fb2bb1e4e88c265b7c35de239
Raman <Raman@DESKTOP-9P221R1>
2020-10-24 02:07:04 +0530

    maven n jenkins

---
+++ src/test/java/demoJava.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class demoJava {
+  @Test
+  public void m() {
+	  //WebDriver driver = new ChromeDriver();
+	  System.out.println("Hello");
+  }
+}
